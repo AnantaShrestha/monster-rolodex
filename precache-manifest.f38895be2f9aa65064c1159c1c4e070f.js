d6ceb127e6766ba03c10f05a245bafc0ccf06b24
AnantaShrestha <anantashrestha01@gmail.com>
2020-08-23 23:15:45 +0545

    Updates

---
+++ precache-manifest.f38895be2f9aa65064c1159c1c4e070f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f1ccee0d821bce1b83aabbab4a7c11",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bd90440188e67f276cbc",
+    "url": "/monster-rolodex/static/css/main.19956274.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd90440188e67f276cbc",
+    "url": "/monster-rolodex/static/js/main.097e7534.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
